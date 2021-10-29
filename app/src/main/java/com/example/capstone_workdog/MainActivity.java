5e1d5f559480ac5e2bd628cb7296c6647e4ea3c8
sohuipark <ck1808@naver.com>
2021-10-29 22:31:14 +0900

    test

--- app/src/main/java/com/example/capstone_workdog/MainActivity.java
+++ app/src/main/java/com/example/capstone_workdog/MainActivity.java
@@ -15,11 +15,25 @@ public class MainActivity extends TabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost tabHost = getTabHost();
-        TabHost.TabSpec spec;
+        //↓사용하려면 fragment나... 기타 다른 activity extend 필요
+//        getSupportFragmentManager().beginTransaction()
+//                .replace(android.R.id.content, new setting(), null).commit();
 
-        tabHost.addTab(tabHost.newTabSpec("setting_tab").setIndicator("setting")
-        .setContent(new Intent(this,setting.class)));
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabspecmap = tabHost.newTabSpec("map").setIndicator("맵");
+        tabspecmap.setContent(R.id.map);
+        tabHost.addTab(tabspecmap);
+
+        TabHost.TabSpec tabspeccommu = tabHost.newTabSpec("community").setIndicator("커뮤니티");
+        tabspeccommu.setContent(R.id.community);
+        tabHost.addTab(tabspeccommu);
+
+        //탭에서 액티비티를 사용할 수 있도록 인텐트 생성
+        Intent intent = new Intent().setClass(this,setting.class);
+        TabHost.TabSpec tabspecset = tabHost.newTabSpec("setting").setIndicator("설정"); //객체 생성 및 이름 설정
+        tabspecset.setContent(intent);
+        tabHost.addTab(tabspecset);
 
         tabHost.setCurrentTab(0);
     }
--- app/src/main/java/com/example/capstone_workdog/setting.java
+++ app/src/main/java/com/example/capstone_workdog/setting.java
@@ -1,14 +1,26 @@
 package com.example.capstone_workdog;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragmentCompat;
 
-import androidx.annotation.Nullable;
-
-public class setting extends PreferenceFragment {
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+public class setting extends PreferenceFragmentCompat {
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+//        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.setting);
+
+
+//        하단 내용은 알림 내부로 들어갈 예정임!
+//                <CheckBoxPreference
+//        android:title="알림 설정"
+//        android:defaultValue="false"
+//        android:key="Ringing"/>
+//        <SwitchPreference
+//        android:key="sound"
+//        android:title="소리"
+//        android:dependency="Ringing"/>
+//        <SwitchPreference
+//        android:key="message"
+//        android:title="진동"
+//        android:dependency="Ringing"/>
     }
 }
